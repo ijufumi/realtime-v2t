1ae4c09ec406fb2e10b10a0ab07ce9d464ef6ee6
Takafumi Iju <ijufumi@gmail.com>
2020-07-17 23:47:00 +0900

    initial commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            '@babel/preset-env',
+                            '@babel/preset-typescript',
+                            '@babel/react'
+                        ],
+                        plugins: [
+                            '@babel/plugin-proposal-class-properties'
+                        ]
+                    }
+                },
+                exclude: /node_modules/
+            },
+            {
+                test: /\.css?/,
+                use: {
+                    loader: "css-loader"
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".css"]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        open: true,
+        port: 9000
+    }
+};
