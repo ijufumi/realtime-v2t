d7121f258b0e46911cd32a5c69c23dc8e7cf498c
Takafumi Iju <ijufumi@gmail.com>
2020-08-01 12:59:01 +0900

    fixed typo.

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ from app.ws import serve
 def run():
     ws_server = websockets.serve(serve, Config.WS_HOST, int(Config.WS_PORT))
     asyncio.get_event_loop().run_until_complete(ws_server)
-    asyncio.get_event_loop().run_forver()
+    asyncio.get_event_loop().run_forever()
 
 
 if __name__ == '__main__':
