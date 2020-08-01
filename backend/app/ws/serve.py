1f592a5675ca14e6fd25620f39f4e5703f0a150e
Takafumi Iju <ijufumi@gmail.com>
2020-08-01 13:12:48 +0900

    add recv/send sample.

--- backend/app/ws/serve.py
+++ backend/app/ws/serve.py
@@ -1,6 +1,17 @@
-import logging
-logging.basicConfig()
+from app.logging import logger
+
+from websockets.exceptions import ConnectionClosedError
 
 
 async def serve(ws, path):
-    pass
+    logger.info("serve start")
+    try:
+        async for message in ws:
+            # name = await ws.recv()
+            logger.info(f"received: {message}")
+            greeting = f"Hello {message}!"
+            await ws.send(greeting)
+    except ConnectionClosedError as e:
+        logger.info("connection closed.", exc_info=e)
+    finally:
+        logger.info("serve end")
