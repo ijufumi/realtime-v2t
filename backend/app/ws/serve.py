d29761399c670d521bafda29061861bf8c0c6d2d
Takafumi Iju <ijufumi@gmail.com>
2020-08-01 13:15:25 +0900

    add feature of managing connected users.

--- backend/app/ws/serve.py
+++ backend/app/ws/serve.py
@@ -2,10 +2,13 @@ from app.logging import logger
 
 from websockets.exceptions import ConnectionClosedError
 
+USERS = set()
+
 
 async def serve(ws, path):
     logger.info("serve start")
     try:
+        register(ws)
         async for message in ws:
             # name = await ws.recv()
             logger.info(f"received: {message}")
@@ -14,4 +17,13 @@ async def serve(ws, path):
     except ConnectionClosedError as e:
         logger.info("connection closed.", exc_info=e)
     finally:
+        unregister(ws)
         logger.info("serve end")
+
+
+def register(ws):
+    USERS.add(ws)
+
+
+def unregister(ws):
+    USERS.remove(ws)
