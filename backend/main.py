74e0221be2193e9f17c11cd1b4ff67bb609cb694
Takafumi Iju <ijufumi@gmail.com>
2020-08-08 13:17:43 +0900

    Add handler of sigint.

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,26 @@
 import asyncio
 import websockets
+import signal
+from websockets.http import Headers
 
 from app.config import Config
 from app.ws import Server
+from app.logging import logger
+
+CORS_HEADERS = Headers([("Access-Control-Allow-Origin", "*")])
+
+
+def stop(signum, frame):
+    logger.info("shutdown...")
+    exit(1)
+
+
+signal.signal(signal.SIGINT,  stop)
 
 
 def run():
     server = Server()
-    ws_server = websockets.serve(server.serve, Config.WS_HOST, int(Config.WS_PORT))
+    ws_server = websockets.serve(server.serve, Config.WS_HOST, int(Config.WS_PORT), origins=["*"])
     asyncio.get_event_loop().run_until_complete(ws_server)
     asyncio.get_event_loop().run_forever()
 
