74e9436886f782a4fd07f8d844ae78e6be911035
Takafumi Iju <ijufumi@gmail.com>
2020-08-10 21:18:48 +0900

    Add sanic to run server.

--- backend/app/ws/serve.py
+++ backend/app/ws/serve.py
@@ -1,26 +1,34 @@
 import socketio
+from sanic import Sanic
 
+from app.config import Config
 from app.logging import logger
 from app.services import S3Service
 
 
-sio = socketio.AsyncServer()
+sio = socketio.AsyncServer(async_mode='sanic')
+app = Sanic()
+sio.attach(app)
 
 
 class Server:
     def __init__(self):
         self.s3_service = S3Service()
 
-    @sio.event
+    @sio.on("connect", namespace="room")
     def connect(self, sid):
         logger.info(f"connected: ${sid}")
         sio.enter_room(sid, 'roos', 'default')
 
-    @sio.event
+    @sio.on("disconnect", namespace="room")
     def disconnect(self, sid):
         logger.info(f"disconnected: ${sid}")
         sio.leave_room(sid, 'room', 'default')
 
-    @sio.event
+    @sio.on("message", namespace="room")
     def message(self, sid, data):
         logger.info(f"received: ${sid}:${data}")
+
+    @staticmethod
+    def run():
+        app.run(host=Config.WS_HOST, port=Config.WS_PORT)
--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,9 @@
-import asyncio
-import signal
-
-from app.config import Config
 from app.ws import Server
-from app.logging import logger
-
-
-def stop(signum, frame):
-    logger.info("shutdown...")
-    exit(1)
-
-
-signal.signal(signal.SIGINT,  stop)
 
 
 def run():
     server = Server()
+    server.run()
 
 
 if __name__ == '__main__':
