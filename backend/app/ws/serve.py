83f7ad505566c0fd2793e054c146b608379ba3c1
Takafumi Iju <ijufumi@gmail.com>
2020-08-12 23:04:58 +0900

    Modify server setting

--- backend/app/ws/serve.py
+++ backend/app/ws/serve.py
@@ -1,4 +1,5 @@
 import socketio
+from typing import Text, Dict, Any
 from sanic import Sanic
 from sanic_cors import CORS
 
@@ -7,29 +8,33 @@ from app.logging import logger
 from app.services import S3Service
 
 
-sio = socketio.AsyncServer(async_mode='sanic', cors_allowed_origins=[])
+manager = socketio.BaseManager()
+sio = socketio.Server(client_manager=manager, binary=True, cors_allowed_origins=[])
+sio.binary = True
 app = Sanic()
-CORS(app, resources={r"/*": {"origins": "*"}})
-sio.attach(app)
+app.config['CORS_AUTOMATIC_OPTIONS'] = True
+app.config['CORS_SUPPORTS_CREDENTIALS'] = True
+CORS(app)
+#sio.attach(app)
 
 
 class Server:
     def __init__(self):
         self.s3_service = S3Service()
 
-    @sio.on("connect", namespace="room")
-    def connect(self, sid):
-        logger.info(f"connected: ${sid}")
-        sio.enter_room(sid, 'roos', 'default')
+    @sio.on("connect")
+    async def connect(self, sid: Text) -> None:
+        logger.info(f"connected: {sid}")
+        #sio.enter_room(sid, 'roos', 'default')
 
-    @sio.on("disconnect", namespace="room")
-    def disconnect(self, sid):
-        logger.info(f"disconnected: ${sid}")
-        sio.leave_room(sid, 'room', 'default')
+    @sio.on("disconnect")
+    async def disconnect(self, sid: Text) -> None:
+        logger.info(f"disconnected: {sid}")
+        #sio.leave_room(sid, 'room', 'default')
 
-    @sio.on("message", namespace="room")
-    def message(self, sid, data):
-        logger.info(f"received: ${sid}:${data}")
+    @sio.event
+    async def message(self, sid: Text, data: Any) -> None:
+        logger.info(f"received: {sid}:{data}")
 
     @staticmethod
     def run():
