d701f28b2288b6c5a3f145822a77ecc2651ea5eb
Takafumi Iju <ijufumi@gmail.com>
2020-08-25 13:05:05 +0900

    Add plugin.

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require('webpack');
 
 module.exports = {
     entry: "./src/index.tsx",
@@ -40,6 +41,11 @@ module.exports = {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.js"
     },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env.REACT_APP_API_ENDPOINT': JSON.stringify(process.env.REACT_APP_API_ENDPOINT)
+        }),
+    ],
     devServer: {
         contentBase: path.join(__dirname, "dist"),
         compress: true,
